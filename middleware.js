5e88b300513528c8590ab5cc22e0a6b6571229fe
PriyankaKerala8 <Priyanka.wrf87@gmail.com>
2025-04-23 03:10:17 +0000

    Path

--- middleware.js
+++ middleware.js
@@ -4,18 +4,12 @@
  * @param {object} res
  * @param {function} next
  */
-function cors(req, res, next) {
-  const origin = req.headers.origin
-
-  // Set the CORS headers
-  res.setHeader('Access-Control-Allow-Origin', origin || '*')
-  res.setHeader('Access-Control-Allow-Methods', 'POST, GET, PUT, DELETE, OPTIONS, XMODIFY')
-  res.setHeader('Access-Control-Allow-Credentials', true)
-  res.setHeader('Access-Control-Max-Age', '86400')
-  res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With, X-HTTP-Method-Override, Content-Type, Accept')
-
-  next()
-}
+exports.cors = function (req, res, next) {
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
+  res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
+  next();
+};
 
 /**
  * Handle errors
